efd4d1519f89f8a6ad605927317d18afc56cdb52
marcusnguyen <marcus.kimlong@gmail.com>
2025-03-28 20:57:57 +1000

    modify front-end boiler plate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <div>
-
-    </div>
-  )
+  return <div></div>;
 }
 
-export default App
+export default App;
