f43174c8b1fd6815ee4d8d1d6b4a015ad036c471
marcusnguyen <marcus.kimlong@gmail.com>
2025-03-30 08:58:29 +1000

    Add fontawesome and implement login form

---
+++ frontend/src/components/global/logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo(props) {
+  return (
+    <div className={"flex justify-center font-extrabold text-3xl mb-12 "}>
+      <div className={"border-2 p-1"}>EzM</div>
+    </div>
+  );
+}
+
+export default Logo;
--- frontend/src/pages/auth.jsx
+++ frontend/src/pages/auth.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Logo from "../components/global/logo";
 
 function Auth(props) {
-  return <div></div>;
+  useState(() => {}, []);
+
+  return (
+    <>
+      <div className="bg-[url(/src/assets/auth-bg.jpg)] h-screen flex items-center justify-center bg-cover bg-no-repeat">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className="rounded-md border-2 border-primary z-10 shadow-2xl bg-base-100 p-16 px-8 max-w-[400px]">
+            <div
+              className={"mb-1 text-xl font-bold underline flex text-primary"}
+            >
+              Login
+            </div>
+            <div className="flex items-center justify-center text-sm pb-2">
+              Welcome to our site, please login or register to getting started
+            </div>
+            <div className="pb-2">
+              <label className="input w-full">
+                <span className="label">
+                  <div className={"text-xl"}>
+                    <i className="fa-solid fa-envelopes"></i>
+                  </div>
+                </span>
+                <input
+                  className={"w-full"}
+                  type="text"
+                  placeholder="name@site.com"
+                />
+              </label>
+            </div>
+
+            <div className="pb-2">
+              <label className="input w-full">
+                <span className="label">
+                  <div className={"text-xl"}>
+                    <i className="fa-solid fa-lock"></i>
+                  </div>
+                </span>
+                <input
+                  className={"w-full "}
+                  type="text"
+                  placeholder="Enter your password"
+                />
+              </label>
+            </div>
+            <div className="pt-3">
+              <button className="btn btn-primary w-full" type={"submit"}>
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Auth;
