7b3acb1ae9107f9a76546a9124166938e8f47391
marcusnguyen <marcus.kimlong@gmail.com>
2025-04-04 10:21:26 +1000

    hotfix db

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -15,7 +15,7 @@ const getOrders = async (req, res) => {
 const getOrderById = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id)
-      .populate("user", "first_name last_name email")
+      .populate("users", "first_name last_name email")
       .lean();
     if (!order) {
       return res.status(404).json({ message: "Order not found" });
